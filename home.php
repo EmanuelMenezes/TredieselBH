27fdffb08b9a56f3aa8a6d9e857f3cb141fde67a
EmanuelMenezes <borgesam.jb@gmail.com>
2021-04-04 17:59:05 -0300

    teste

--- home.php
+++ home.php
@@ -72,7 +72,7 @@ include("./sistema/database.php");
     <div class="banner row justify-content-center">
         <div class="col-lg-7 title myWhite">
             <h1 style="text-transform: uppercase;">
-                Trediesel VDL
+                Trediesel bh
             </h1>
             <h4>
                 Uma parceria que visa atender os clientes com os melhores critérios de
